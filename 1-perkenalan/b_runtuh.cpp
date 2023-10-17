b94c0ac6a3bac6414a6ead99f0f682b0d5035e97
Alwan Wirawan <alwanwrwn@student.uns.ac.id>
2023-10-17 21:50:22 +0700

    update

--- 1-perkenalan/b_runtuh.cpp
+++ 1-perkenalan/b_runtuh.cpp
@@ -5,17 +5,19 @@
 using namespace std;
 
 int main() {
-  int r, c; cin >> r >> c;
+  int r, c;
+  cin >> r >> c;
 
   vector<vector<int> > map;
 
   // Input
-  for (int i = 0; i<r; i++) {
-    int n; cin >> n;
+  for (int i = 0; i < r; i++) {
+    int n;
+    cin >> n;
     vector<int> temp;
-    
-    for (int j = 0; j<c; j++) {
-      temp.insert(temp.begin(), n%10);
+
+    for (int j = 0; j < c; j++) {
+      temp.insert(temp.begin(), n % 10);
       n /= 10;
     }
 
@@ -27,7 +29,7 @@ int main() {
   while (flag == 0) {
     int last = 0;
     // Check and remove complete row
-    for (int i = 0; i<r; i++) {
+    for (int i = 0; i < r; i++) {
       int sum = accumulate(map[i].begin(), map[i].end(), 0);
 
       if (sum == c) {
@@ -37,23 +39,23 @@ int main() {
     }
 
     // Falling logic (greedy)
-  
+
     // -> 5 ACC, 5 WA
-    for (int i = last; i>= 0; i--) {
-      for (int j = 0; j<c; j++) {
+    for (int i = last; i >= 0; i--) {
+      for (int j = 0; j < c; j++) {
         if (map[i][j] == 1) {
-  
+
           int index = -1;
-          for (int x = i+1; x < r; x++) {
+          for (int x = i + 1; x < r; x++) {
             if (map[x][j] == 1) {
               index = x;
               break;
             }
           }
 
-          if (index>-1) {
+          if (index > -1) {
             map[i][j] = 0;
-            map[index-1][j] = 1;
+            map[index - 1][j] = 1;
           }
         }
       }
@@ -61,7 +63,7 @@ int main() {
 
     // Check
     flag = 1;
-    for (int i = 0; i<r; i++) {
+    for (int i = 0; i < r; i++) {
       int sum = accumulate(map[i].begin(), map[i].end(), 0);
 
       if (sum == c) {
@@ -70,10 +72,10 @@ int main() {
       }
     }
   }
-    
-  for (int i = 0; i<r; i++) {
-    for (int j = 0; j<c; j++) {
-      cout << map[i][j];   
+
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      cout << map[i][j];
     }
     cout << "\n";
   }
--- 1-perkenalan/b_runtuh_2.cpp
+++ 1-perkenalan/b_runtuh_2.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-int main(){
-  int r,c; cin >> r >> c;
+int main() {
+  int r, c;
+  cin >> r >> c;
 
   char map[21][9];
 
-  for (int i = 1; i<=r; i++) {
-    for (int j = 1; j<=c; j++) {
+  for (int i = 1; i <= r; i++) {
+    for (int j = 1; j <= c; j++) {
       cin >> map[i][j];
     }
   }
@@ -19,11 +20,11 @@ int main(){
     int count = 0;
     int lastRow = -1;
 
-    for (int i = 1; i<=r; i++) {
+    for (int i = 1; i <= r; i++) {
       bool allBlack = true;
 
-      for (int j = 1; j<=c; j++) {
-        if (map[i][j] == 0) {
+      for (int j = 1; j <= c; j++) {
+        if (map[i][j] == '0') {
           allBlack = false;
           break;
         }
@@ -31,8 +32,8 @@ int main(){
 
       if (allBlack == true) {
         lastRow = i;
-        for (int j = 1; j<=c; j++) {
-          map[i][j] = 0;
+        for (int j = 1; j <= c; j++) {
+          map[i][j] = '0';
         }
       } else {
         count++;
@@ -40,7 +41,34 @@ int main(){
     }
 
     if (lastRow != -1) {
+      for (int j = 1; j <= c; j++) {
+        int blackCount = 0;
 
+        for (int k = 1; k <= lastRow; k++) {
+          if (map[k][j] == '1') {
+            blackCount++;
+          }
+        }
+
+        if (blackCount > 0) {
+          int destination;
+
+          for (int k = lastRow; k <= r; k++) {
+            if (map[k][j] == '1') {
+              destination = k - 1;
+              break;
+            }
+          }
+
+          for (int k = destination; k >= destination - blackCount; k--) {
+            map[k][j] = '1';
+          }
+
+          for (int k = destination - blackCount; k >= 0; k--) {
+            map[k][j] = '0';
+          }
+        }
+      }
     }
 
     if (count == r) {
@@ -48,8 +76,8 @@ int main(){
     }
   }
 
-  for (int i = 1; i<=r; i++) {
-    for (int j = 1; j<=c; j++) {
+  for (int i = 1; i <= r; i++) {
+    for (int j = 1; j <= c; j++) {
       cout << map[i][j];
     }
     cout << endl;
